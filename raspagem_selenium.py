7700cc4fe4cad5b5e1cf54d31d2314180f32f5e2
MathNovak <106567371+MathNovak@users.noreply.github.com>
2023-01-31 23:36:15 -0300

    init

--- raspagem_selenium.py
+++ raspagem_selenium.py
@@ -6,12 +6,12 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 opcoes = Options()
-#opcoes.add_argument('--headless')
+opcoes.add_argument('--headless') # Executar em segundo plano
 navegador = Chrome(service=Service(ChromeDriverManager().install()), options=opcoes)
 navegador.set_window_size(1280, 1050)
 navegador.maximize_window()
 
-navegador.get('http://raspagem.herokuapp.com/contato/')
+navegador.get('http://raspagem.herokuapp.com/contato/') # Site para preencher campos automaticamente com python usando a ferramenta selenium
 
 time.sleep(1)
 link_contato = navegador.find_element(By.LINK_TEXT, 'Contato')
@@ -23,9 +23,9 @@ campo_nome = navegador.find_element(By.ID, 'id_name')
 campo_email = navegador.find_element(By.ID, 'id_email')
 campo_mensagem = navegador.find_element(By.ID, 'id_message')
 
-campo_nome.send_keys('Matheus Novak')
-campo_email.send_keys('math.ariodante@gmail.com')
-campo_mensagem.send_keys('Sou lindo')
+campo_nome.send_keys('Seu nome')
+campo_email.send_keys('Seu email')
+campo_mensagem.send_keys('Sua mensasgem')
 
 time.sleep(1)
 
